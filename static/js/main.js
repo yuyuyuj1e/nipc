4761f0cc76e401569bd65d280c8da41d0b807538
yuyuyuj1e <807152541@qq.com>
2023-03-15 13:02:08 +0800

    navbar

--- static/js/main.js
+++ static/js/main.js
@@ -260,7 +260,7 @@
     },
     zoom: {
       enabled: true,
-      duration: 300 // don't forget to change the duration also in CSS
+      duration: 300 // don't foget to change the duration also in CSS
     }
   });
 
